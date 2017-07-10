cf5b0874fb00f35a65834315bfd6e80e2c9ee7a0
Arpit Sondh <arpit_sondh@yahoo.com>
2017-07-10 20:16:19 +0530

    Image Depiction of a user's post.

--- InstaBot.py
+++ InstaBot.py
@@ -2,6 +2,8 @@ import requests
 import urllib
 from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
+from clarifai.rest import ClarifaiApp
+
 
 APP_ACCESS_TOKEN = '1460590625.b4a2325.64a3386400124dfa93f31e777e6a0aae'
 
@@ -200,6 +202,31 @@ def delete_negative_comment(insta_username):
     else:
         print 'Status code other than 200 received!'
 
+def image_depiction(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+
+            app = ClarifaiApp(api_key='a9a2f1b7b8034a5baceb72f61a584b18')
+            model = app.models.get("general-v1.3")
+            response = model.predict_by_url(url=image_url)
+            print response
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+    
+
+
 
 def start_bot():
     while True:
@@ -210,12 +237,11 @@ def start_bot():
         print "b.Get details of a user by username\n"
         print "c.Get your own recent post\n"
         print "d.Get the recent post of a user by username\n"
-        #print "e.Get a list of people who have liked the recent post of a user\n"
-        print "f.Like the recent post of a user\n"
-        #print "g.Get a list of comments on the recent post of a user\n"
-        print "h.Make a comment on the recent post of a user\n"
-        print "i.Delete negative comments from the recent post of a user\n"
-        print "j.Exit"
+        print "e.Like the recent post of a user\n"
+        print "f.Make a comment on the recent post of a user\n"
+        print "g.Delete negative comments from the recent post of a user\n"
+        print "h.Image Depiction\n"
+        print "i.Exit"
 
         choice = raw_input("Enter you choice: ")
         if choice == "a":
@@ -228,22 +254,20 @@ def start_bot():
         elif choice == "d":
             insta_username = raw_input("Enter the username of the user: ")
             get_user_post(insta_username)
-        #elif choice=="e":
-           #insta_username = raw_input("Enter the username of the user: ")
-           #get_like_list(insta_username)
-        elif choice=="f":
+        elif choice == "e":
            insta_username = raw_input("Enter the username of the user: ")
            like_a_post(insta_username)
-        #elif choice=="g":
-           #insta_username = raw_input("Enter the username of the user: ")
-           #get_comment_list(insta_username)
-        elif choice=="h":
+        elif choice == "f":
            insta_username = raw_input("Enter the username of the user: ")
            post_a_comment(insta_username)
-        elif choice=="i":
+        elif choice == "g":
            insta_username = raw_input("Enter the username of the user: ")
            delete_negative_comment(insta_username)
-        elif choice == "j":
+        elif choice == "h":
+            insta_username = raw_input("Enter the username of the user: ")
+            image_depiction(insta_username)
+
+        elif choice == "i":
             exit()
         else:
             print "wrong choice"
